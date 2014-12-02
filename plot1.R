a1c300e41659b02423f507ff01a493227fb852f9
Fleksas <Sauliusltu@gmail.com>
2014-12-02 21:56:25 +0200

    Final version of the Coursework

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ source("read_data.R")
 data  <- read_data()
 
 # Creating the first plot
-png("plot1.png", width = 480, height = 480)
+png("plot1.png", width = 480, height = 480) 
 
 hist(data$Global_active_power,col="red",main = "Global Active Power",xlab= "Global Active Power (kilowatts)")
 
