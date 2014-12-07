1b25e061249df69d3c5b2d0dc3b9435b3378b4c4
Fleksas <Sauliusltu@gmail.com>
2014-12-08 00:31:29 +0200

    Final version of the Coursework

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@ source("read_data.R")
 # Reading data from the working ditectory
 data  <- read_data()
 
-# Creating the second plot (3 steps)
+# Creating and saving the second plot 
 png("plot2.png", width = 480, height = 480)
 
 plot(data$Time,data$Global_active_power, type="l",ylab = "Global Active Power (kilowatts)", xlab="")
